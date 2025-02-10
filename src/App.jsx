103231238ec2984fdc7038d6266c94dbc116af7f
Vitdcode <vitalijdietrich@gmail.com>
2025-02-10 11:37:16 +0100

    Feat: added more categories to the shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
-import FetchItem from "./fetch-components/FetchItem";
+import FetchItemsByCategory from "./fetch-components/FetchItem";
 
 function App() {
   const [products, setProducts] = useState([]);
-  const category = "electronics";
+  const [category, setCategory] = useState("default");
 
   return (
     <div className="h-screen flex">
       <div className="flex flex-col justify-start border-r-2 border-gray-200 w-[600px]">
         <h2 className="m-10">Category</h2>
-        <h2 className="m-10">Electronics</h2>
+        <button onClick={() => setCategory("electronics")}>Electronics</button>
+        <button onClick={() => setCategory("jewelery")}>Jewelery</button>
+        <button onClick={() => setCategory(`men's clothing`)}>{"Men's clothing"}</button>
+        <button onClick={() => setCategory(`women's clothing`)}>{"women's clothing"}</button>
       </div>
-      <div className="flex items-center">
-        <FetchItem category={category} products={products} setProducts={setProducts} />
+      <div className="flex items-center justify-center flex-wrap gap-20 w-full">
+        <FetchItemsByCategory
+          category={category}
+          setCategory={setCategory}
+          products={products}
+          setProducts={setProducts}
+        />
       </div>
     </div>
   );
--- src/fetch-components/FetchItem.jsx
+++ src/fetch-components/FetchItem.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
 
-export default function FetchItemsByCategory({ category, products, setProducts }) {
+export default function FetchItemsByCategory({ category, setCategory, products, setProducts }) {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (category === "default") {
+      setCategory("electronics");
+      return;
+    }
     fetch(`https://fakestoreapi.com/products/category/${category}`, { mode: "cors" })
       .then((response) => response.json())
       .then((products) => {
-        console.log(products);
         setProducts(products);
       })
       .catch((error) => {
         setError(error);
       })
       .finally(() => setLoading(false));
-  }, []);
+  }, [category]);
 
   if (loading) return <h1>...Loading Page</h1>;
   if (error) return <h1>The Page encountered an error, please try again later</h1>;
@@ -24,9 +27,9 @@ export default function FetchItemsByCategory({ category, products, setProducts }
     products && (
       <>
         {products.map((product) => (
-          <div key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <h2>{product.title}</h2>
+          <div key={product.id} className="w-[200px] flex flex-col justify-center">
+            <img src={product.image} alt={product.title} className="mx-auto " />
+            <h2 className="overflow-hidden text-ellipsis whitespace-nowrap">{product.title}</h2>
           </div>
         ))}
       </>
